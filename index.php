46a8a4bbf3eef71b2e47d60577851d73ae21e897
shivamsuyal <shivamsuyal25@gmail.com>
2022-12-05 11:58:30 +0530

    edit1.2

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-// require_once "pages/mobile/home.php";
 require_once "vendor/mobiledetect/mobiledetectlib/src/MobileDetect.php";
 $detect = new \Detection\MobileDetect;
 
 if ( $detect->isMobile() ) {
+    require_once "pages/mobile/home.php";
    }else{
     require_once "pages/desktop/home.php";
 }
